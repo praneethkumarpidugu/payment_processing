061f789d2ba037d1652812de9295c2c23e3808f9
praneethkumarpidugu <pidugu2007@gmail.com>
2015-07-11 16:46:35 -0400

    changed the homepage url and configured static files at settings.py, urls.py

--- payment_processing/urls.py
+++ payment_processing/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from djang.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'payment_processing.views.home', name='home'),
+    url(r'^$', 'profiles.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^anotherurlpattern/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- profiles/models.py
+++ profiles/models.py
@@ -5,8 +5,7 @@ from django.db import models
 class profile(models.Model):
     name = models.CharField(max_length=1200)
     description = models.TextField(default='description default')
-    location = models.CharField(max_length=1200, default='My Location')
-    job = models.CharField(max_length=1200, null=True)
+
 
     def __unicode__(self):
         return self.name
