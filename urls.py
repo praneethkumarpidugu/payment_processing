f22309dbc13e9c660c56699284eb7fded30ad2ce
praneethkumarpidugu <pidugu2007@gmail.com>
2015-07-11 23:06:53 -0400

    created an app contact, added views, added templates

--- urls.py
+++ urls.py
@@ -7,7 +7,6 @@ urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'profiles.views.home', name='home'),
     url(r'^about/$', 'profiles.views.about', name='about'),
-    url(r'^contact/$', 'contact.views.contact', name='contact'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^anotherurlpattern/', include(admin.site.urls)),
